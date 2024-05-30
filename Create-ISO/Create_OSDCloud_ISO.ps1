628c5616574b1f8e97fd23556973d7d8bddf4b58
nickj1976 <117663799+nickj1976@users.noreply.github.com>
2024-05-30 15:01:22 +0100

    created script to create OSDCloud ISO

---
+++ Create-ISO/Create_OSDCloud_ISO.ps1
@@ -0,0 +1,28 @@
+# Install deployment tools: https://go.microsoft.com/fwlink/?linkid=2120254
+# Install WinPE add-on for Windows ADK: https://go.microsoft.com/fwlink/?linkid=2120253
+# Install Module: Install-Module OSD -Force
+
+# English/GB Template
+New-OSDCloudTemplate -Language en-GB -SetInputLocale en-GB -Verbose
+
+# Set Workshpace Folder
+$WorkingDir = "C:\OSDCloud"
+New-Item -ItemType Directory $WorkingDir
+Set-OSDCloudWorkspace -WorkspacePath $WorkingDir
+
+$Startnet = @'
+start /wait PowerShell -NoL -C Install-Module OSD -Force -Verbose
+start /wait PowerShell -NoL -C Start-OSDCloud -OSVersion 'Windows 11' -OSBuild 23H2 -OSEdition Pro -OSLanguage en-GB -OSLicense Retail
+'@
+Edit-OSDCloudWinPE -Startnet $Startnet -StartOSDCloudGUI -Brand 'University of Surrey' -Wallpaper "C:\temp\Surrey_2023.png" -CloudDriver Dell,WiFi,USB,IntelNet
+
+# create OSD ISO
+New-OSDCloudISO -WorkspacePath $WorkingDir
+Write-Host "ISO Created: $WorkingDir" 
+
+<# 
+#Uninstall
+Get-Module -ListAvailable -Name OSD
+Remove-Module OSD
+Uninstall-Module OSD -AllVersions -Force -Verbose
+#>
